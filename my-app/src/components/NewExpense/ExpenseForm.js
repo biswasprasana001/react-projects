197792cdc3f4ed6697daf5d073819558e89ced14
biswasprasana001 <biswasprasana004@gmail.com>
2021-12-13 21:29:58 +0530

    having a bug error, will be fixing it

---
+++ my-app/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,24 @@
+const ExpenseForm = () => {
+    return (
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type = 'text' />
+                </div>
+                <div className="new-expense__control">
+                    <label>Amount</label>
+                    <input type = 'number' min = '0.01' step = '0.01' />
+                </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type = 'date' min = '2020-01-01' max = '2021-12-31'/>
+                </div>
+            </div>
+            <div className="new-expense__actions"></div>
+            <button type="submit">Add Expense</button>
+        </form>
+    );
+};
+
+export default ExpenseForm;
