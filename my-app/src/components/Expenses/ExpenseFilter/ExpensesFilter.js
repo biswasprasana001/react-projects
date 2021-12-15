370c37324080b01469b7a010fd5ec759f33d52ef
biswasprasana001 <biswasprasana004@gmail.com>
2021-12-15 21:21:41 +0530

    will be continued

--- my-app/src/components/Expenses/ExpenseFilter/ExpensesFilter.js
+++ my-app/src/components/Expenses/ExpenseFilter/ExpensesFilter.js
@@ -5,12 +5,13 @@ import "./ExpensesFilter.css";
 const ExpensesFilter = (props) => {
   const changeHandler = (event) => {
     props.onSelectYear(event.target.value);
+    console.log(event.target.value);
   };
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select onChange={changeHandler}>
+        <select value={props.selectValue} onChange={changeHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
