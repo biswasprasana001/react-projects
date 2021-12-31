bc0754a8ec30a5ccdd252050630d46d1699d9ff3
biswasprasana001 <biswasprasana004@gmail.com>
2021-12-31 19:10:13 +0530

    Added a message if Expense Items are not displayed

--- my-app-01/src/components/Expenses/Expenses.js
+++ my-app-01/src/components/Expenses/Expenses.js
@@ -9,22 +9,27 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
   const filteredExpenses = props.items.filter((expense) => {
-    return expense.date.getFullYear().toString()===filteredYear;
+    return expense.date.getFullYear().toString() === filteredYear;
   });
+
+  let expensesContent = <p>No Expenses Found</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expensesContent}
     </Card>
   );
 }
