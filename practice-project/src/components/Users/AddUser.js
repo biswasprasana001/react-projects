71dd294f3d190ec0ff495638d08a22a86e515a3b
biswasprasana001 <biswasprasana004@gmail.com>
2022-01-07 20:06:36 +0530

    To Find  A Bug

--- practice-project/src/components/Users/AddUser.js
+++ practice-project/src/components/Users/AddUser.js
@@ -7,14 +7,21 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const[error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-      return;
+      setError ({
+        title: 'Invalid Input',
+        message: 'Please Enter a Valid Name & Age (Non-Empty Values)'
+      })
     }
     if (+enteredAge < 1) {
-      return;
+      setEnteredAge({
+        title: 'Invalid Age',
+        message: 'Please Enter a Valid Age (>0)'
+      })
     }
     props.onAddUser(enteredUsername, enteredAge);
     setEnteredUsername("");
@@ -31,7 +38,7 @@ const AddUser = (props) => {
 
   return (
     <div>
-      <ErrorModal title="An Error Occured!" message="Something Went Wrong!" />
+      {error && <ErrorModal title={error.title} message={error.message} />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">UserName</label>
