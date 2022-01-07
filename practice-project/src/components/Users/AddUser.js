272b33f0d3cc7d49f28dae8428fbf65b5d945ee5
biswasprasana001 <biswasprasana004@gmail.com>
2022-01-07 21:06:07 +0530

    Project Completed

--- practice-project/src/components/Users/AddUser.js
+++ practice-project/src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import React, { useState } from "react";
+
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
@@ -7,21 +8,23 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
-  const[error, setError] = useState();
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError ({
-        title: 'Invalid Input',
-        message: 'Please Enter a Valid Name & Age (Non-Empty Values)'
-      })
+title: 'Invalid Input',
+        message: 'Please Enter a Valid Name & Age (Non-Empty Values)',
+      });
+      return;
     }
     if (+enteredAge < 1) {
-      setEnteredAge({
+      setError({
         title: 'Invalid Age',
-        message: 'Please Enter a Valid Age (>0)'
-      })
+        message: 'Please Enter a Valid Age (>0)',
+      });
+      return;
     }
     props.onAddUser(enteredUsername, enteredAge);
     setEnteredUsername("");
@@ -36,9 +39,13 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      {error && <ErrorModal title={error.title} message={error.message} />}
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">UserName</label>
