dd8f0993f4454177a720241dacc9087f542b272f
biswasprasana001 <biswasprasana004@gmail.com>
2022-01-30 02:04:56 +0530

    Will Be Fixing Bug

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,17 +1,16 @@
-import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = "Welcome to the New Blog";
-  const likes = 50;
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{10 + 10}</p>
+        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
-  );
+  ); 
 }
 
 export default App;
