1ac02d83b3803fbef458c48efc8a38368265d282
biswasprasana001 <biswasprasana004@gmail.com>
2022-02-02 19:44:17 +0530

    Will Be Fixing Bug

--- code-15-react-projects-john-smilga/12-sidebar-modal/setup/src/Home.js
+++ code-15-react-projects-john-smilga/12-sidebar-modal/setup/src/Home.js
@@ -1,13 +1,18 @@
 import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
+import { AppContext, useGlobalContext } from "./context";
 
 const Home = () => {
+  const { openSideBar, openModal } = useGlobalContext();
+
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSideBar}>
         <FaBars />
       </button>
-      <button className="btn">Show Modal</button>
+      <button className="btn" onClick={openModal}>
+        Show Modal
+      </button>
     </main>
   );
 };
--- code-15-react-projects-john-smilga/12-sidebar-modal/setup/src/context.js
+++ code-15-react-projects-john-smilga/12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,38 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSideBar = () => {
+    setIsSideBarOpen(true);
+  };
+
+  const closeSideBar = () => {
+    setIsSideBarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsSideBarOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsSideBarOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={(openSideBar, closeSideBar, openModal, closeModal, isModalOpen, isSideBarOpen)}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
