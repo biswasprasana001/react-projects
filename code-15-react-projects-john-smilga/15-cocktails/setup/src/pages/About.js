039287928e8bfb5c74a53e62d927aa2696048bce
biswasprasana001 <biswasprasana004@gmail.com>
2022-02-06 18:55:21 +0530

    Done For Now, Will Be Doing Next, Later

--- code-15-react-projects-john-smilga/15-cocktails/setup/src/pages/About.js
+++ code-15-react-projects-john-smilga/15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
+        similique optio doloribus voluptatum incidunt, dicta nostrum iusto
+        facilis culpa sunt dolores deserunt ea eaque excepturi, fuga quae et?
+        Sequi consequuntur voluptatum exercitationem odio velit similique
+        excepturi, ipsum veniam itaque quos.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
