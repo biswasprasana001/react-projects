e5b2a7c350f9181064331eeb638effb39ea1fdba
biswasprasana001 <biswasprasana004@gmail.com>
2022-03-30 17:18:53 +0530

    Will have to see where the error is

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -6,13 +6,12 @@ import Tours from "./Tours";
 const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState([]);
-
+  const [tours, setTours] = useState();
   const fetchTours = async () => {
-    setLoading(true);
     try {
+      setLoading(true);
       const response = await fetch(url);
-      const tours = await response.json;
+      const tours = await response.json();
       setLoading(false);
       setTours(tours);
     } catch (error) {
@@ -28,7 +27,7 @@ function App() {
   if (loading) {
     return (
       <main>
-        <Loading />;
+        <Loading />
       </main>
     );
   }
