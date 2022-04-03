05c367f02e96bce7df10871f0391f8ba59251822
biswasprasana001 <biswasprasana004@gmail.com>
2022-04-04 00:09:47 +0530

    Done with Resetting

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,32 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { FaAngleDoubleRight } from "react-icons/fa";
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = "https://course-api.com/react-tabs-project";
+const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [jobs, setJobs] = useState([]);
-  const [value, setValue] = useState(0);
-
-  const fetchJobs = async () => {
-    const response = await fetch(url);
-    const newJobs = await response.json();
-    setLoading(false);
-    setJobs(newJobs);
-  };
-
-  useEffect(() => fetchJobs(), []);
-
-  if (loading) {
-    return (
-      <section className="section loading">
-        <h1>loading...</h1>
-      </section>
-    );
-  }
-
-  const { company, dates, duties, title } = jobs[value];
-  return <section className="section"></section>;
+  return <h2>tabs project setup</h2>
 }
 
-export default App;
+export default App
