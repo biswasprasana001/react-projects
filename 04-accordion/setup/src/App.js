7c11ae347d33d84293c12dd94115d87efeca3025
biswasprasana001 <biswasprasana004@gmail.com>
2022-04-03 11:39:00 +0530

    We reset Everything as of now

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,20 +1,8 @@
-import React, { useState } from "react";
-import data from "./data";
-import SingleQuestion from "./Question";
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
 function App() {
-  const [questions, setQuestions] = useState(data);
-  return (
-    <main>
-      <div className="container">
-        <h3>questions and answers about login</h3>
-        <section>
-          {questions.map((question) => {
-            return <SingleQuestion key={question.id} {...question} />;
-          })}
-        </section>
-      </div>
-    </main>
-  );
+  return <h2>accordion project setup</h2>;
 }
 
 export default App;
