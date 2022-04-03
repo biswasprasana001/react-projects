2d18e7bbb2627b3f39847786e1a3e956372a9172
biswasprasana001 <biswasprasana004@gmail.com>
2022-04-03 19:54:05 +0530

    Okay, let's see where to debug

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -9,62 +9,24 @@ function App() {
   const [value, setValue] = useState(0);
 
   const fetchJobs = async () => {
-    const reponse = await fetch(url);
-    const newJobs = await reponse.json();
-    setJobs(newJobs);
+    const response = await fetch(url);
+    const newJobs = await response.json();
     setLoading(false);
+    setJobs(newJobs);
   };
 
-  useEffect(() => {
-    fetchJobs();
-  }, []);
+  useEffect(() => fetchJobs(), []);
 
   if (loading) {
     return (
       <section className="section loading">
-        <h1>Loading...</h1>
+        <h1>loading...</h1>
       </section>
     );
   }
 
   const { company, dates, duties, title } = jobs[value];
-  return (
-    <section className="section">
-      <div className="title">
-        <h2>experience</h2>
-        <div className="underline"></div>
-      </div>
-      <div className="jobs-center">
-        <div className="btn-container">
-          {jobs.map((item, index) => {
-            return (
-              <button
-                key={item.id}
-                onClick={() => setValue(index)}
-                className={`job-btn ${index === value && "active-btn"}`}
-              >
-                {item.company}
-              </button>
-            );
-          })}
-        </div>
-        <article className="job-info">
-          <h3>{title}</h3>
-          <h4>{company}</h4>
-          <p className="job-date">{dates}</p>
-          {duties.map((duty, index) => {
-            return (
-              <div className="job-desc">
-                <FaAngleDoubleRight className="job-icon" />
-                <p>{duty}</p>
-              </div>
-            );
-          })}
-        </article>
-      </div>
-      <button className="btn">more info</button>
-    </section>
-  );
+  return <section className="section"></section>;
 }
 
 export default App;
