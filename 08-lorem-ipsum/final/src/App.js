cc3c28116e4ce4211c1f64205db35bb456353a42
biswasprasana001 <biswasprasana004@gmail.com>
2022-04-06 22:32:50 +0530

    Back to Reset

--- 08-lorem-ipsum/final/src/App.js
+++ 08-lorem-ipsum/final/src/App.js
@@ -1,41 +1,35 @@
-import React, { useState } from "react";
-import data from "./data";
+import React, { useState } from 'react';
+import data from './data';
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
-  const [isValid, setIsValid] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     let amount = parseInt(count);
-    if (count < 0) {
+    if (count <= 0) {
       amount = 1;
     }
     if (count > 8) {
       amount = 8;
     }
-    setIsValid(false);
     setText(data.slice(0, amount));
-    if (amount === 0) {
-      setIsValid(true);
-    }
   };
   return (
-    <section className="section-center">
+    <section className='section-center'>
       <h3>tired of boring lorem ipsum?</h3>
-      <form className="lorem-form" onSubmit={handleSubmit}>
-        <label htmlFor="amount">paragraphs:</label>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
         <input
-          type="number"
-          name="amount"
-          id="amount"
+          type='number'
+          name='amount'
+          id='amount'
           value={count}
           onChange={(e) => setCount(e.target.value)}
         />
-        <button className="btn">generate</button>
+        <button className='btn'>generate</button>
       </form>
-      <article className="lorem-text">
-        {isValid && <p>Enter Valid No</p>}
+      <article className='lorem-text'>
         {text.map((item, index) => {
           return <p key={index}>{item}</p>;
         })}
