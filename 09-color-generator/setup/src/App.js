47fd32573d201095a85e57788dfc323d6447c12a
biswasprasana001 <biswasprasana004@gmail.com>
2022-04-08 15:56:05 +0530

    Back to Start

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,54 +1,10 @@
-import React, { useState } from "react";
-import SingleColor from "./SingleColor";
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
 
-import Values from "values.js";
+import Values from 'values.js'
 
 function App() {
-  const [color, setColor] = useState("");
-  const [list, setList] = useState(new Values("#4287f5").all(10));
-  const [error, setError] = useState(false);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    try {
-      let colors = new Values(color).all(10);
-      setError(false);
-      setList(colors);
-    } catch (error) {
-      setError(true);
-      console.log(error);
-    }
-  };
-  return (
-    <>
-      <section className="container">
-        <h3>color generator</h3>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="eg. #32a852"
-            onChange={(e) => setColor(e.target.value)}
-            value={color}
-            className={error ? `error` : null}
-          />
-          <button className="btn" type="submit">
-            submit
-          </button>
-        </form>
-      </section>
-      <section className="colors">
-        {list.map((color, index) => {
-          return (
-            <SingleColor
-              key={index}
-              index={index}
-              {...color}
-              hexColor={color.hex}
-            />
-          );
-        })}
-      </section>
-    </>
-  );
+  return <h2>color generator project</h2>
 }
 
-export default App;
+export default App
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,30 +1,8 @@
-import React, { useState, useEffect } from "react";
-import rgbToHex from "./utils";
+import React, { useState, useEffect } from 'react'
+import rgbToHex from './utils'
 
-const SingleColor = ({ rgb, weight, index, hexColor }) => {
-  const [alert, setAlert] = useState(false);
-  const hexValue = `#${hexColor}`;
-  const bcg = rgb.join(",");
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setAlert(false);
-    }, 1000);
-    return () => clearTimeout(timeout);
-  }, [alert]);
-  return (
-    <article
-      style={{ backgroundColor: `rgb(${bcg})` }}
-      onClick={() => {
-        setAlert(true);
-        navigator.clipboard.writeText(hexValue);
-      }}
-      className={`color ${index > 7 ? "color-light" : null}`}
-    >
-      <p className="percent-value">{weight}%</p>
-      <p className="color-value">{hexValue}</p>
-      {alert && <p className="alert">copied to clipboard</p>}
-    </article>
-  );
-};
+const SingleColor = () => {
+  return <h4>single color</h4>
+}
 
-export default SingleColor;
+export default SingleColor
