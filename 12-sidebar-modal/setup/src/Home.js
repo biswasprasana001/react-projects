37dfda614f77532b00e1378765a2f697da31935d
biswasprasana001 <biswasprasana004@gmail.com>
2022-04-13 20:08:37 +0530

    Everything reseted

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,19 +1,8 @@
-import React, { useContext } from "react";
-import { FaBars } from "react-icons/fa";
-import { AppContext, useGlobalContext } from "./context";
+import React, { useContext } from 'react'
+import { FaBars } from 'react-icons/fa'
 
 const Home = () => {
-  const { openSidebar, openModal } = useGlobalContext();
-  return (
-    <main>
-      <button className="sidebar-toggle" onClick={openSidebar}>
-        <FaBars />
-      </button>
-      <button className="btn" onClick={openModal}>
-        show modal
-      </button>
-    </main>
-  );
-};
+  return <h2>home component</h2>
+}
 
-export default Home;
+export default Home
