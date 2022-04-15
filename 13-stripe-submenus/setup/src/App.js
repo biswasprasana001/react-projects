eb90c5250492acf6676e37c7a29246f1e6e5b445
biswasprasana001 <biswasprasana004@gmail.com>
2022-04-15 19:33:54 +0530

    Everything reseted

--- 13-stripe-submenus/setup/src/App.js
+++ 13-stripe-submenus/setup/src/App.js
@@ -1,16 +1,14 @@
-import React from "react";
-import Navbar from "./Navbar";
-import Hero from "./Hero";
-import Sidebar from "./Sidebar";
-import Submenu from "./Submenu";
+import React from 'react'
+import Navbar from './Navbar'
+import Hero from './Hero'
+import Sidebar from './Sidebar'
+import Submenu from './Submenu'
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Sidebar />
+      <h2>stripe submenus setup</h2>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
