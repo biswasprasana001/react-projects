34db1557cdb8ad3c7e5cb8cb5235c8df0e61f173
biswasprasana001 <biswasprasana004@gmail.com>
2022-04-15 19:33:11 +0530

    Done with the first half of the project

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,26 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
+import React from "react";
+import { useGlobalContext } from "./context";
+import phoneImg from "./images/phone.svg";
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Millions of businesses of all sizes—from startups to large
+            enterprises—use Stripe's software and APIs to accept payments, send
+            payouts, and manage their businesses online.
+          </p>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} alt="phone" className="phone-img" />
+        </article>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
