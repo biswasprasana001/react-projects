8a81ccf73e067bfadcb4075de5cb687293a120a9
biswasprasana001 <biswasprasana004@gmail.com>
2022-05-07 20:22:41 +0530

    Updated the Version of npm packages

--- 15-cocktails/setup/src/App.js
+++ 15-cocktails/setup/src/App.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-// import pages
-import Home from './pages/Home'
-import About from './pages/About'
-import SingleCocktail from './pages/SingleCocktail'
-import Error from './pages/Error'
-// import components
-import Navbar from './components/Navbar'
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
