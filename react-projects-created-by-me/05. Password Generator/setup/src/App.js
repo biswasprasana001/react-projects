fc5626517105177b3464ec3a6d7b3409872e2e48
biswasprasana001 <biswasprasana004@gmail.com>
2022-07-18 16:29:27 +0530

    Project Completed, Onto Intermediate Ones

--- react-projects-created-by-me/05. Password Generator/setup/src/App.js
+++ react-projects-created-by-me/05. Password Generator/setup/src/App.js
@@ -1,13 +1,69 @@
-import { MdContentCopy } from "react-icons/md";
+import { useState } from "react";
+import {
+  numbers,
+  upperCaseLetters,
+  lowerCaseLetters,
+  specialCharacters,
+} from "./Character";
 
 function App() {
+  const [copiedToClipboard, setCopiedToClipboard] = useState(false);
+  const [password, setPassword] = useState("");
+  const [passwordLength, setPasswordLength] = useState(26);
+  const [includeUpperCase, setIncludeUpperCase] = useState(false);
+  const [includeLowerCase, setIncludeLowerCase] = useState(false);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeSymbols, setIncludeSymbols] = useState(false);
+  const handleCopy = () => {
+    setCopiedToClipboard(true);
+    navigator.clipboard.writeText(password);
+  };
+  const handlePassword = (e) => {
+    e.preventDefault();
+    setCopiedToClipboard(false);
+    if (
+      !includeUpperCase &&
+      !includeLowerCase &&
+      !includeNumbers &&
+      !includeSymbols
+    ) {
+      alert("You Must Atleast Select One Checkbox!!");
+    } else {
+      let charList = "";
+      if (includeNumbers) {
+        charList += numbers;
+      }
+      if (includeUpperCase) {
+        charList += upperCaseLetters;
+      }
+      if (includeLowerCase) {
+        charList += lowerCaseLetters;
+      }
+      if (includeSymbols) {
+        charList += specialCharacters;
+      }
+      setPassword(createPassword(charList));
+    }
+  };
+  const createPassword = (charList) => {
+    let password = "";
+    for (let i = 0; i < passwordLength; i++) {
+      const randomIndex = Math.round(Math.random() * charList.length);
+      password += charList.charAt(randomIndex);
+    }
+    return password;
+  };
   return (
     <div className="container">
       <div className="generator">
         <h2 className="header">Password Generator</h2>
-        <div className="password">
-          <h4>abcdefghijklmnopqrstuvwxyz</h4>
-          <MdContentCopy />
+        <div className="passwordContainer">
+          <h4 className="the-password">{password}</h4>
+          {password && (
+            <button className="btn-copy" onClick={() => handleCopy()}>
+              {copiedToClipboard ? `Copied` : ` Copy`}
+            </button>
+          )}
         </div>
         <form className="form">
           <div className="form-group">
@@ -19,6 +75,8 @@ function App() {
               name="password-strength"
               max="26"
               min="8"
+              onChange={(e) => setPasswordLength(e.target.value)}
+              value={passwordLength}
             />
           </div>
           <div className="form-group">
@@ -27,6 +85,8 @@ function App() {
               type="checkbox"
               id="uppercase-letters"
               name="uppercase-letters"
+              checked={includeUpperCase}
+              onChange={(e) => setIncludeUpperCase(e.target.checked)}
             />
           </div>
           <div className="form-group">
@@ -35,6 +95,8 @@ function App() {
               type="checkbox"
               id="lowercase-letters"
               name="lowercase-letters"
+              checked={includeLowerCase}
+              onChange={(e) => setIncludeLowerCase(e.target.checked)}
             />
           </div>
           <div className="form-group">
@@ -43,6 +105,8 @@ function App() {
               type="checkbox"
               id="include-numbers"
               name="include-numbers"
+              checked={includeNumbers}
+              onChange={(e) => setIncludeNumbers(e.target.checked)}
             />
           </div>
           <div className="form-group">
@@ -51,9 +115,13 @@ function App() {
               type="checkbox"
               id="include-symbols"
               name="include-symbols"
+              checked={includeSymbols}
+              onChange={(e) => setIncludeSymbols(e.target.checked)}
             />
           </div>
-          <button className="btn">Generate Password</button>
+          <button className="btn" onClick={handlePassword}>
+            Generate Password
+          </button>
         </form>
       </div>
     </div>
